92e7ec8db138ceb6fa10596747dd73175e5f5355
GRealesM <grealesm@gmail.com>
2023-07-19 18:13:05 +0100

    bhattacharyya 7pcs only

--- code/compute-bhattacharyyan-distance.R
+++ code/compute-bhattacharyyan-distance.R
@@ -15,6 +15,10 @@ library(cupcake)
 ps <- fread("../data/ps2.tsv")
 qs <- fread("../data/qs2.tsv")
 
+# Key features
+pcs <- paste0("PC", c(1,2,3,8,9,12,13))
+
+
 
 # Since files are in hg38 and the basis is in hg19, we use a dirty fix to liftover back to hg19 (easier to do at this stage than in the previous one, due to the huge size of files).
 build_dict <- fread(paste0(bpath, "Manifest_build_translator.tsv"))
@@ -45,6 +49,7 @@ covar.m <- lapply(fs, function(x){
         v <- seb * shrinkage[pids] * rot.pca[pids,]
         var.proj  <- t(v) %*% LD[pids,pids] %*% v
 
+        var.proj <- var.proj[pcs, pcs]
         var.proj
 })
 
@@ -54,16 +59,16 @@ bh.list <- lapply(seq_along(qs$Label), function(i){
                 dd <- data.table(T1 = qs$Label[i], T2 = qs$Label) # Create a data.table to store combinations
 
                 # Data for focus trait (ie. T1)
-                rr1  <- ps[ Label == qs$Label[i], Delta]
-                names(rr1) <- ps[ Label == qs$Label[i], PC]
+                rr1  <- ps[ Label == qs$Label[i] & PC %in% pcs, Delta]
+                names(rr1) <- ps[ Label == qs$Label[i] & PC %in% pcs, PC]
 
                 cv1 <- as.matrix(covar.m[[i]])
                 stopifnot(row.names(cv1) == names(rr1))
 
                 # Then, take each trait in turn, extract necessary data, and compute the Bhattacharyya distance
                 bhd <- sapply(seq_along(qs$Label), function(x){
-                        rr2 <- ps[ Label == qs$Label[x], Delta]
-                        names(rr2) <- ps[ Label == qs$Label[x], PC] 
+                        rr2 <- ps[ Label == qs$Label[x] & PC %in% pcs, Delta]
+                        names(rr2) <- ps[ Label == qs$Label[x] & PC %in% pcs, PC] 
                         cv2 <- as.matrix(covar.m[[x]])
                         stopifnot(row.names(cv1) == names(rr2))
                         bd <- bhattacharyya.dist(mu1 = rr1, mu2 = rr2, Sigma1 = cv1, Sigma2 = cv2 )
