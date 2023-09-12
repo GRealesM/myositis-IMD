b135336462d8811929750981c69b6a64b2ab2e0d
GRealesM <grealesm@gmail.com>
2023-09-12 16:01:39 +0100

    coloc viz results

---
+++ code/07-Extract-SNPs-to-map.R
@@ -0,0 +1,78 @@
+#########################################
+##                                     ##
+##  EXTRACTING DRIVER AND CANDIDATES   ##
+##                                     ##
+#########################################
+
+# Author: Guillermo Reales 
+# Date last updated: 2023/09/12
+
+# Background: After finishing coloc, we'll need to extract the chromosomes, rsids, alleles, etc. from the relevant driver and bestsnps.
+# Then we'll use 08-Mapping-genes.py on the generated file to extract the names of the nearest genes.
+
+# This script is meant to be run at the HPC
+setwd("/home/gr440/rds/rds-cew54-basis/Projects/myositis-IMD/code")
+
+
+#########################################
+
+
+# Load packages
+library(data.table)
+setDTthreads(15)
+library(magrittr)
+
+# Load data
+cc <- fread("../data/coloc_results.tsv")
+
+snps <- c(cc[ H4 > 0.5, unique(pid)],cc[ H4 > 0.5, unique(bestsnp)]) %>% unique
+
+# We'll extract all info from the DM (M) dataset
+mm <- fread("~/rds/rds-cew54-basis/02-Processed/MYO_Miller_26291516_1-hg38.tsv.gz")
+mr <- fread("~/rds/rds-cew54-basis/02-Processed/IIM_Rothwell_up_1-hg38.tsv.gz")
+mm[, pid:=paste(CHR38, BP38, sep=":")]
+mr[, pid:=paste(CHR38, BP38, sep=":")]
+mm <- mm[pid %in% snps, .(pid, CHR38, BP38, SNPID, REF, ALT)]
+mr <- mr[pid %in% snps, .(pid, CHR38, BP38, SNPID, REF, ALT)]
+all(snps %in% mm$pid) # Not all SNPs present
+setdiff(snps, mm$pid) # Locate SNP
+mr[pid %in% setdiff(snps, mm$pid)] # is it in the alternative dataset? It is
+mm <- rbindlist(list(mm, mr[pid %in% setdiff(snps, mm$pid)])) # add missing snp to dataset
+mm[pid == "8:11484361", SNPID:="rs2736336"] # add missing rsid
+
+names(mm)[2:4] <- c("CHR", "BP", "SNP")
+
+
+# Save
+fwrite(mm, "../data/snp.to.map.tsv", sep="\t")
+
+#########################################
+
+sessionInfo()
+# R version 4.3.1 (2023-06-16)
+# Platform: x86_64-redhat-linux-gnu (64-bit)
+# Running under: Rocky Linux 8.8 (Green Obsidian)
+
+# Matrix products: default
+# BLAS/LAPACK: /usr/lib64/libopenblaso-r0.3.15.so;  LAPACK version 3.9.0
+
+# locale:
+#  [1] LC_CTYPE=en_GB.UTF-8       LC_NUMERIC=C              
+#  [3] LC_TIME=en_GB.UTF-8        LC_COLLATE=en_GB.UTF-8    
+#  [5] LC_MONETARY=en_GB.UTF-8    LC_MESSAGES=en_GB.UTF-8   
+#  [7] LC_PAPER=en_GB.UTF-8       LC_NAME=C                 
+#  [9] LC_ADDRESS=C               LC_TELEPHONE=C            
+# [11] LC_MEASUREMENT=en_GB.UTF-8 LC_IDENTIFICATION=C       
+
+# time zone: GB
+# tzcode source: system (glibc)
+
+# attached base packages:
+# [1] stats     graphics  grDevices utils     datasets  methods   base     
+
+# other attached packages:
+# [1] magrittr_2.0.3    data.table_1.14.8
+
+# loaded via a namespace (and not attached):
+# [1] compiler_4.3.1    cli_3.6.1         R.methodsS3_1.8.2 jsonlite_1.8.7   
+# [5] R.utils_2.12.2    rlang_1.1.1       R.oo_1.25.0 
